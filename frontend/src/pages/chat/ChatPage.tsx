d3b859918a49b4d554ec133bfba0416a4feccf6b
Bhagya <bhagyaranjan992@gmail.com>
2026-06-24 10:01:37 +0530

    ui

--- frontend/src/pages/chat/ChatPage.tsx
+++ frontend/src/pages/chat/ChatPage.tsx
@@ -90,7 +90,7 @@ export default ChatPage;
 
 const NoConversationPlaceholder = () => (
 	<div className='flex flex-col items-center justify-center h-full space-y-6'>
-		<img src='/syncwave.png' alt='Spotify' className='size-16 animate-bounce' />
+		<img src='/syncwave.png' alt='Spotify' className='size-16 animate-bounce rounded-full' />
 		<div className='text-center'>
 			<h3 className='text-zinc-300 text-lg font-medium mb-1'>No conversation selected</h3>
 			<p className='text-zinc-500 text-sm'>Choose a friend to start chatting</p>
